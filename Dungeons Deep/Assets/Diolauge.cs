dda5ba94e49f23bf05fc54eddb98b54a9be0fd6d
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2019-01-27 15:00:51 -0800

    Add Graphics for Diolauge + some code

---
+++ Dungeons Deep/Assets/Diolauge.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Diolauge
+{
+
+    public string name;
+
+    [TextArea(3, 10)]
+    public string[] sentences;
+
+}
