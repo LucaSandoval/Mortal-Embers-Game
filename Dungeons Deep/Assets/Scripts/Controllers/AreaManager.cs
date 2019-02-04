c70a0facacae0eb9550155934d3e860ced4d9887
Luca Sandoval <32229145+LucaSandoval@users.noreply.github.com>
2019-02-04 14:33:57 -0800

    Update Area Title

---
+++ Dungeons Deep/Assets/Scripts/Controllers/AreaManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaManager : MonoBehaviour {
+
+    public int areaID = 1;
+    public int emberID = 1;
+    private string areaName = "null";
+    private string emberName = "null";
+
+    private Text areNameText;
+    private Text emberNameText;
+
+    private void Start()
+    {
+        areNameText = GameObject.FindGameObjectWithTag("areaText").GetComponent<Text>();
+        emberNameText = GameObject.FindGameObjectWithTag("emberText").GetComponent<Text>();
+
+
+        switch (areaID)
+        {
+            case 1:
+                areaName = "Stonefall Tower";
+                break;
+            case 2:
+                areaName = "Mortal Garden";
+                break;
+            case 3:
+                areaName = "Old Keep of Embers";
+                break;
+            default:
+                areaName = "null";
+                break;
+        }
+
+        switch (emberID)
+        {
+            case 1:
+                emberName = "Ember of Dark";
+                break;
+            case 2:
+                emberName = "Ember of Wrath";
+                break;
+            case 3:
+                emberName = "Ember of Faith";
+                break;
+            default:
+                emberName = "null";
+                break;
+        }
+
+
+        emberNameText.text = emberName;
+        areNameText.text = areaName;
+
+    }
+}
